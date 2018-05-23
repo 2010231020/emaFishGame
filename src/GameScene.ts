d4a5e011db4731e23f5277db47e2e9640cd4031b
kbaoqq <389565552@qq.com>
2018-05-23 22:11:10 +0800

    git push test

--- src/GameScene.ts
+++ src/GameScene.ts
@@ -119,71 +119,71 @@ class GameScene extends eui.Component {
     }
 
     private init(scale): void {
-        let gene = GameLogic.getInstance().gameData[this.fishId].gene;
-        this.x = 50 * (1 + 5 * Math.random());//X范围50-300
-        this.y = 80 * (1 + 4 * Math.random());//y范围80-400
-        // this.scaleX = scale;
-        // this.scaleY = scale;
-        this.anchorOffsetX = 200 * scale;
-        this.anchorOffsetY = 200 * scale;
-        // this.anchorOffsetX = 200*0.4;
-        // this.anchorOffsetY = 200*0.4;
-        this.rotation = 180 * (-1 + Math.random());
-        //确定主题色
-        let bodyObj = {
-            "1": "3",
-            "2": "2",
-            "3": "1",
-            "4": "5",
-            "5": "6",
-            "6": "4"
-        }
-        this.type = bodyObj[gene[0]];
-        //尾巴
-        this.Body5 = new Body5(this.type, this.deData(gene[5]), scale);
-        this.addChild(this.Body5);
-        //身体和眼睛
-        this.Fish = new Fish(this.type, this.deData(gene[1]), scale);
-        this.addChild(this.Fish);
-        //左眼
-        // this.Body2 = new Body2(this.deData(gene[1]));
-        // this.addChild(this.Body2);
-        //背鳍
-        this.Body3 = new Body3(this.type, this.deData(gene[2]), scale);
-        this.addChild(this.Body3);
-        //左翅
-        this.Body4 = new Body4(this.type, this.deData(gene[3]), scale);
-        this.addChild(this.Body4);
-        //胡须
-        this.Body6 = new Body6(this.type, this.deData(gene[4]), scale);
-        this.addChild(this.Body6);
-        //右眼
-        // this.Body7 = new Body7(this.deData(gene[1]));
-        // this.addChild(this.Body7);
-        //右翅
-        this.Body8 = new Body8(this.type, this.deData(gene[3]), scale);
-        // let bubbleText = new egret.Bitmap(RES.getRes(`bubble_text_png`));
-        // this.addChild(bubbleText);
-        this.addChild(this.Body8);
-        if (GameLogic.getInstance().gameData[this.fishId].travelUid) {
-            let textField = new egret.TextField();
-            this.addChild(textField);
-            textField.x = 0;
-            textField.width = 400 * scale;
-            textField.height = 100;
-            textField.textColor = 0xff0000;
-            textField.text = "访客";
-            textField.textAlign = "center";
-            textField.size = 12;
-        }
-        this.swim();
-        this.addEventListener(egret.TouchEvent.TOUCH_TAP, event => {
-            let msg = {
-                type: GameLogic.getInstance().gameData[this.fishId].travelUid ? 2 : 1,//1:自家鱼；2:访客鱼
-                fishId: GameLogic.getInstance().gameData[this.fishId].fishId
-            };
-            GameLogic.getInstance().sendMsg('show', msg);
-        }, this);
+        // let gene = GameLogic.getInstance().gameData[this.fishId].gene;
+        // this.x = 50 * (1 + 5 * Math.random());//X范围50-300
+        // this.y = 80 * (1 + 4 * Math.random());//y范围80-400
+        // // this.scaleX = scale;
+        // // this.scaleY = scale;
+        // this.anchorOffsetX = 200 * scale;
+        // this.anchorOffsetY = 200 * scale;
+        // // this.anchorOffsetX = 200*0.4;
+        // // this.anchorOffsetY = 200*0.4;
+        // this.rotation = 180 * (-1 + Math.random());
+        // //确定主题色
+        // let bodyObj = {
+        //     "1": "3",
+        //     "2": "2",
+        //     "3": "1",
+        //     "4": "5",
+        //     "5": "6",
+        //     "6": "4"
+        // }
+        // this.type = bodyObj[gene[0]];
+        // //尾巴
+        // this.Body5 = new Body5(this.type, this.deData(gene[5]), scale);
+        // this.addChild(this.Body5);
+        // //身体和眼睛
+        // this.Fish = new Fish(this.type, this.deData(gene[1]), scale);
+        // this.addChild(this.Fish);
+        // //左眼
+        // // this.Body2 = new Body2(this.deData(gene[1]));
+        // // this.addChild(this.Body2);
+        // //背鳍
+        // this.Body3 = new Body3(this.type, this.deData(gene[2]), scale);
+        // this.addChild(this.Body3);
+        // //左翅
+        // this.Body4 = new Body4(this.type, this.deData(gene[3]), scale);
+        // this.addChild(this.Body4);
+        // //胡须
+        // this.Body6 = new Body6(this.type, this.deData(gene[4]), scale);
+        // this.addChild(this.Body6);
+        // //右眼
+        // // this.Body7 = new Body7(this.deData(gene[1]));
+        // // this.addChild(this.Body7);
+        // //右翅
+        // this.Body8 = new Body8(this.type, this.deData(gene[3]), scale);
+        // // let bubbleText = new egret.Bitmap(RES.getRes(`bubble_text_png`));
+        // // this.addChild(bubbleText);
+        // this.addChild(this.Body8);
+        // if (GameLogic.getInstance().gameData[this.fishId].travelUid) {
+        //     let textField = new egret.TextField();
+        //     this.addChild(textField);
+        //     textField.x = 0;
+        //     textField.width = 400 * scale;
+        //     textField.height = 100;
+        //     textField.textColor = 0xff0000;
+        //     textField.text = "访客";
+        //     textField.textAlign = "center";
+        //     textField.size = 12;
+        // }
+        // this.swim();
+        // this.addEventListener(egret.TouchEvent.TOUCH_TAP, event => {
+        //     let msg = {
+        //         type: GameLogic.getInstance().gameData[this.fishId].travelUid ? 2 : 1,//1:自家鱼；2:访客鱼
+        //         fishId: GameLogic.getInstance().gameData[this.fishId].fishId
+        //     };
+        //     GameLogic.getInstance().sendMsg('show', msg);
+        // }, this);
     }
 
     private swim(): void {
